2a5749dd55375c6448fc850ed7b17b12030901d3
Renato Montanher <renato.montanher@fatec.sp.gov.br>
2016-06-20 21:18:56 -0300

    Observações sobre modificador de acesso Public

--- Chapter_2/src/graphicshape/Circle.java
+++ Chapter_2/src/graphicshape/Circle.java
@@ -3,7 +3,7 @@ package graphicshape;
 public class Circle extends Shape {
 	private int radius; // private field
 
-	public void area() { // public method
+	public void area() { // public method: acessivel em qualquer outro lugar
 		// access to private field radius inside the class:
 		System.out.println("area: " + 3.14 * radius * radius);
 	}
@@ -14,3 +14,6 @@ public class Circle extends Shape {
 		System.out.println("color: " + color);
 	}
 }
+//para um método publico ser acessivel de qualquer lugar é necessário que a CLASSE 
+//TAMBEM SEJA PÚBLICA, do contrário, a classe sendo acessivel como default e 
+// o metodo so ficara disonivel para ser acessado no proprio pacote
